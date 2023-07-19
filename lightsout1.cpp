1464930e40448a917d31d581ef7d5dab14bcc295
Ryogo Tannai <ryogotannai@Oya-labnoiMac.local>
2023-07-19 16:12:45 +0900

    anything

--- lightsout1.cpp
+++ lightsout1.cpp
@@ -1,20 +1,27 @@
 #include <fstream>
 #include <iostream>
 #include <ostream>
+#include <set>
 #include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+void print_vector(vector<int> vec) {
+  for (auto v : vec)
+    cout << v;
+  cout << endl;
+}
+
 vector<vector<int>> get_laplacian_matrix(int H, int W) {
   int N = H * W;
   vector<vector<int>> A(N, vector<int>(N, false));
   for (int i = 0; i < N; ++i) {
     A[i][i] = 1;
-    if (i % H != 0)
+    if (i % W != 0)
       A[i][i - 1] = 1;
-    if ((i + 1) % H != 0)
+    if ((i + 1) % W != 0)
       A[i][i + 1] = 1;
     if (i + W < N)
       A[i][i + W] = 1;
@@ -26,14 +33,14 @@ vector<vector<int>> get_laplacian_matrix(int H, int W) {
 
 tuple<vector<vector<int>>, vector<vector<int>>>
 get_stair_matrix_inverse_matrix(vector<vector<int>> A) {
-  int m = A.size();
-  vector<vector<int>> B(m, vector<int>(m, 0));
-  for (int i = 0; i < m; ++i)
+  int N = A.size();
+  vector<vector<int>> B(N, vector<int>(N, 0));
+  for (int i = 0; i < N; ++i)
     B[i][i] = 1;
 
-  for (int i = 0; i < m; ++i) {
+  for (int i = 0; i < N; ++i) {
     if (!A[i][i]) {
-      for (int j = i + 1; j < m; ++j) {
+      for (int j = i + 1; j < N; ++j) {
         if (A[j][i]) {
           swap(A[i], A[j]);
           swap(B[i], B[j]);
@@ -41,9 +48,9 @@ get_stair_matrix_inverse_matrix(vector<vector<int>> A) {
         }
       }
     }
-    for (int j = i + 1; j < m; ++j) {
+    for (int j = i + 1; j < N; ++j) {
       if (A[j][i]) {
-        for (int k = 0; k < m; ++k) {
+        for (int k = 0; k < N; ++k) {
           A[j][k] ^= A[i][k];
           B[j][k] ^= B[i][k];
         }
@@ -51,13 +58,13 @@ get_stair_matrix_inverse_matrix(vector<vector<int>> A) {
     }
   }
 
-  for (int i = m - 1; i >= 0; --i) {
+  for (int i = N - 1; i >= 0; --i) {
     bool ok = false;
     if (!A[i][i])
       continue;
     for (int j = i - 1; j >= 0; --j) {
       if (A[j][i]) {
-        for (int k = 0; k < m; ++k) {
+        for (int k = 0; k < N; ++k) {
           A[j][k] ^= A[i][k];
           B[j][k] ^= B[i][k];
         }
@@ -69,10 +76,10 @@ get_stair_matrix_inverse_matrix(vector<vector<int>> A) {
 }
 
 vector<int> matrix_times_vector(vector<vector<int>> A, vector<int> b) {
-  int m = A.size();
-  vector<int> res(m, 0);
-  for (int i = 0; i < m; ++i) {
-    for (int j = 0; j < m; ++j) {
+  int N = A.size();
+  vector<int> res(N, 0);
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
       res[i] ^= A[i][j] * b[j];
     }
   }
@@ -82,10 +89,11 @@ vector<int> matrix_times_vector(vector<vector<int>> A, vector<int> b) {
 vector<vector<int>> matrix_times_matrix(vector<vector<int>> A,
                                         vector<vector<int>> B) {
 
-  vector<vector<int>> C(A.size(), vector<int>(A.size(), 0));
-  for (int i = 0; i < A.size(); ++i) {
-    for (int j = 0; j < A.size(); ++j) {
-      for (int k = 0; k < A.size(); ++k) {
+  int N = A.size();
+  vector<vector<int>> C(N, vector<int>(N, 0));
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      for (int k = 0; k < N; ++k) {
         C[i][j] ^= B[i][k] * A[k][j];
       }
     }
@@ -94,70 +102,67 @@ vector<vector<int>> matrix_times_matrix(vector<vector<int>> A,
 }
 
 int get_corank(vector<vector<int>> A) {
-  for (int i = 0; i < A.size(); ++i) {
+  int N = A.size();
+  for (int i = 0; i < N; ++i) {
     bool ok = true;
-    for (int j = 0; j < A.size(); ++j)
+    for (int j = 0; j < N; ++j)
       if (A[i][j])
         ok = false;
     if (ok)
-      return A.size() - i;
+      return N - i;
   }
   return 0;
 }
 
-int get_1st_coefficient(vector<vector<int>> B, int corank) {
-  int n = B.size();
-  int cnt = 0;
-  for (int i = 0; i < n; ++i) {
-    vector<int> x(n, 0);
-    x[i] = 1;
-    x = matrix_times_vector(B, x);
-    bool ok = true;
-    for (int j = n - corank; j < n; ++j)
-      if (x[j])
-        ok = false;
-    if (ok)
-      cnt++;
-  }
-  return cnt;
-}
+set<vector<int>> get_kernel_vectors(vector<vector<int>> A, int corank) {
+  int N = A.size();
+  set<vector<int>> res;
+  vector<int> tmp(N, 0);
+  res.insert(tmp);
 
-int get_2nd_coefficient(vector<vector<int>> B, int corank) {
-  int n = B.size();
-  int cnt = 0;
-  for (int i = 0; i < n - 1; ++i) {
-    for (int j = i + 1; j < n; ++j) {
-      vector<int> x(n, 0);
-      x[i] = 1;
-      x[j] = 1;
-      x = matrix_times_vector(B, x);
-      bool ok = true;
-      for (int k = n - corank; k < n; ++k)
-        if (x[k])
-          ok = false;
-      if (ok)
-        cnt++;
+  set<vector<int>> kernel_base_vectors;
+  for (int i = N - corank; i < N; ++i) {
+    vector<int> v(N, 0);
+    for (int j = 0; j < i - 1; ++j) {
+      if (A[j][i])
+        v[j] = 1;
     }
+    v[i] = 1;
+    kernel_base_vectors.insert(v);
   }
-  return cnt;
+
+  for (auto v : kernel_base_vectors) {
+    set<vector<int>> st_tmp;
+    for (auto s : res) {
+      vector<int> tmp(N, 0);
+      for (int i = 0; i < N; ++i) {
+        tmp[i] = s[i] ^ v[i];
+      }
+      st_tmp.insert(tmp);
+      st_tmp.insert(s);
+    }
+    res = st_tmp;
+  }
+  return res;
 }
 
-void solve_all_ones(int H, int W, vector<vector<int>> inverse_matrix) {
+set<vector<int>> solve_all_ones(int H, int W,
+                                vector<vector<int>> inverse_matrix,
+                                vector<vector<int>> stair_matrix, int corank) {
   int N = H * W;
   vector<int> all_ones(N, 1);
-  vector<int> solve_matrix = matrix_times_vector(inverse_matrix, all_ones);
-  vector<vector<int>> tmp(H, vector<int>(W));
-  for (int i = 0; i < H; ++i) {
-    for (int j = 0; j < W; ++j) {
-      tmp[i][j] = solve_matrix[i * H + j];
-    }
+  vector<int> solve_vector = matrix_times_vector(inverse_matrix, all_ones);
+  cout << "solve" << endl;
+  print_vector(solve_vector);
+  set<vector<int>> solve_vector_set;
+  set<vector<int>> kernek_vectors = get_kernel_vectors(stair_matrix, corank);
+  for (auto vec : kernek_vectors) {
+    vector<int> tmp(N);
+    for (int i = 0; i < N; ++i)
+      tmp[i] = vec[i] ^ solve_vector[i];
+    solve_vector_set.insert(tmp);
   }
-  for (auto vec : tmp) {
-    for (auto val : vec) {
-      cout << val;
-    }
-    cout << endl;
-  }
+  return solve_vector_set;
 }
 
 void write_csv(int i, int A_1, int A_2, int corank) {
@@ -175,13 +180,15 @@ int main() {
       vector<vector<int>> B;
       tie(A, B) = get_stair_matrix_inverse_matrix(A);
       int corank = get_corank(A);
-      /*
-      set<vector<vector<int>> solves;
-      solves = solve_all_ones(i, j, B);
-      */
-      cout << "H" << i << "W" << j << endl;
-      cout << "corank" << corank << endl;
-      solve_all_ones(i, j, B);
+      set<vector<int>> solves;
+      solves = solve_all_ones(i, j, B, A, corank);
+      cout << "H " << i << " ,W:" << j << ", size:" << solves.size() << endl;
+      for (auto vec : solves)
+        print_vector(vec);
+      set<vector<int>> st = get_kernel_vectors(A, corank);
+      cout << "kernel" << endl;
+      for (auto vec : st)
+        print_vector(vec);
       cout << endl;
     }
   }
