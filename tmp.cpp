152a5299a197fd1e3284565b5dfc47689efaab1a
Ryogo Tannai <ryogotannai@Oya-labnoiMac.local>
2023-07-19 16:14:31 +0900

    anything

---
+++ tmp.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define PB push_back
+#define MP make_pair
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define FOR(i, n, m) for (int i = (n); i < (m); ++i)
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+int main() { cout << 0 % 1 << endl; }
